8db9ce43b06b734753e51d076e6d6b29d40946b4
coherencedemo <164892412+coherencedemo@users.noreply.github.com>
2024-08-22 13:14:15 +0200

    Update Database URL

--- ebdjango/settings.py
+++ ebdjango/settings.py
@@ -1,5 +1,6 @@
-import os
 from dotenv import load_dotenv
+import os
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -59,10 +60,7 @@ WSGI_APPLICATION = 'ebdjango.wsgi.application'
 
 # Database
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
+    'default': dj_database_url.config(default=os.getenv('DATABASE_URL'))
 }
 
 # Password validation
